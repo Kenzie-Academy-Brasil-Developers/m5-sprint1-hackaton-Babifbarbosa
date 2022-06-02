712126caf3c7684602b5ffa7896e743d8fbea2d3
Felipe Braga Alfredo <felipebraga@macbook-air-de-felipe.home>
2022-06-02 14:59:37 +0200

    initial commit

--- hackaton.py
+++ hackaton.py
@@ -1,1 +1,11 @@
 # Desenvolva seu código abaixo
+hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
+hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
+hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
+
+
+def get_score (team_name, teams): 
+    return f'A {team_name} ficou classificada em {teams.index(team_name) + 1} lugar.'
+
+get_score_1 = get_score("Team VHSYS", hackathon_3)
+print(get_score_1)
